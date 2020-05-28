3a461acab66261c1a6f6bd1ba9f328cf763b4042
dani <dani.zaghian@gmail.com>
2020-05-28 09:59:29 -0700

    adds query params

--- app/controllers/api/examples_controller.rb
+++ app/controllers/api/examples_controller.rb
@@ -1,2 +1,9 @@
 class Api::ExamplesController < ApplicationController
+
+  def query_params_action
+    @message = "The message is #{params['message']}"
+    @second_message = "The second message is #{params['second_message']}"
+    render "query_params.json.jb"
+  end
+
 end
